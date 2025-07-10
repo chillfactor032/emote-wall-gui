5744d9a52910c5f9df10f93a86086b5b38a429ee
chillfactor032 <chill@chillaspect.com>
2025-07-10 00:14:23 -0500

    UI Updates

--- ui.py
+++ ui.py
@@ -15,9 +15,12 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QGridLayout, QGroupBox, QHBoxLayout,
-    QMainWindow, QMenuBar, QSizePolicy, QStackedWidget,
-    QStatusBar, QVBoxLayout, QWidget)
+from PySide6.QtWidgets import (QApplication, QComboBox, QFormLayout, QGridLayout,
+    QGroupBox, QHBoxLayout, QLabel, QLineEdit,
+    QMainWindow, QMenuBar, QSizePolicy, QSpacerItem,
+    QSpinBox, QStackedWidget, QStatusBar, QToolButton,
+    QVBoxLayout, QWidget)
+import Resources_rc
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
@@ -36,6 +39,232 @@ class Ui_MainWindow(object):
         self.verticalLayout.setContentsMargins(0, 0, 0, 0)
         self.groupBox_2 = QGroupBox(self.widget)
         self.groupBox_2.setObjectName(u"groupBox_2")
+        self.verticalLayout_2 = QVBoxLayout(self.groupBox_2)
+        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
+        self.widget_9 = QWidget(self.groupBox_2)
+        self.widget_9.setObjectName(u"widget_9")
+        font = QFont()
+        font.setStyleStrategy(QFont.PreferDefault)
+        self.widget_9.setFont(font)
+        self.formLayout = QFormLayout(self.widget_9)
+        self.formLayout.setObjectName(u"formLayout")
+        self.formLayout.setFormAlignment(Qt.AlignLeading|Qt.AlignLeft|Qt.AlignVCenter)
+        self.formLayout.setContentsMargins(0, 0, 0, 0)
+        self.widget_3 = QWidget(self.widget_9)
+        self.widget_3.setObjectName(u"widget_3")
+        self.horizontalLayout_3 = QHBoxLayout(self.widget_3)
+        self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
+        self.label_2 = QLabel(self.widget_3)
+        self.label_2.setObjectName(u"label_2")
+        sizePolicy = QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.label_2.sizePolicy().hasHeightForWidth())
+        self.label_2.setSizePolicy(sizePolicy)
+        self.label_2.setMinimumSize(QSize(24, 24))
+        self.label_2.setPixmap(QPixmap(u":/resources/img/icons/twitch.svg"))
+        self.label_2.setScaledContents(True)
+
+        self.horizontalLayout_3.addWidget(self.label_2)
+
+        self.label = QLabel(self.widget_3)
+        self.label.setObjectName(u"label")
+
+        self.horizontalLayout_3.addWidget(self.label)
+
+
+        self.formLayout.setWidget(0, QFormLayout.LabelRole, self.widget_3)
+
+        self.widget_5 = QWidget(self.widget_9)
+        self.widget_5.setObjectName(u"widget_5")
+        self.gridLayout_3 = QGridLayout(self.widget_5)
+        self.gridLayout_3.setObjectName(u"gridLayout_3")
+        self.lineEdit = QLineEdit(self.widget_5)
+        self.lineEdit.setObjectName(u"lineEdit")
+        sizePolicy1 = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
+        sizePolicy1.setHorizontalStretch(0)
+        sizePolicy1.setVerticalStretch(0)
+        sizePolicy1.setHeightForWidth(self.lineEdit.sizePolicy().hasHeightForWidth())
+        self.lineEdit.setSizePolicy(sizePolicy1)
+        self.lineEdit.setMinimumSize(QSize(0, 24))
+        font1 = QFont()
+        font1.setPointSize(11)
+        self.lineEdit.setFont(font1)
+
+        self.gridLayout_3.addWidget(self.lineEdit, 0, 0, 1, 1)
+
+
+        self.formLayout.setWidget(0, QFormLayout.FieldRole, self.widget_5)
+
+        self.widget_4 = QWidget(self.widget_9)
+        self.widget_4.setObjectName(u"widget_4")
+        self.horizontalLayout_4 = QHBoxLayout(self.widget_4)
+        self.horizontalLayout_4.setObjectName(u"horizontalLayout_4")
+        self.label_3 = QLabel(self.widget_4)
+        self.label_3.setObjectName(u"label_3")
+        sizePolicy.setHeightForWidth(self.label_3.sizePolicy().hasHeightForWidth())
+        self.label_3.setSizePolicy(sizePolicy)
+        self.label_3.setMinimumSize(QSize(24, 24))
+        self.label_3.setPixmap(QPixmap(u":/resources/img/icons/twitch.svg"))
+        self.label_3.setScaledContents(True)
+
+        self.horizontalLayout_4.addWidget(self.label_3)
+
+        self.label_4 = QLabel(self.widget_4)
+        self.label_4.setObjectName(u"label_4")
+
+        self.horizontalLayout_4.addWidget(self.label_4)
+
+
+        self.formLayout.setWidget(1, QFormLayout.LabelRole, self.widget_4)
+
+        self.widget_6 = QWidget(self.widget_9)
+        self.widget_6.setObjectName(u"widget_6")
+        self.gridLayout_4 = QGridLayout(self.widget_6)
+        self.gridLayout_4.setObjectName(u"gridLayout_4")
+        self.lineEdit_2 = QLineEdit(self.widget_6)
+        self.lineEdit_2.setObjectName(u"lineEdit_2")
+        self.lineEdit_2.setMinimumSize(QSize(0, 24))
+        self.lineEdit_2.setFont(font1)
+        self.lineEdit_2.setEchoMode(QLineEdit.Password)
+
+        self.gridLayout_4.addWidget(self.lineEdit_2, 0, 0, 1, 1)
+
+
+        self.formLayout.setWidget(1, QFormLayout.FieldRole, self.widget_6)
+
+        self.widget_8 = QWidget(self.widget_9)
+        self.widget_8.setObjectName(u"widget_8")
+        self.horizontalLayout_6 = QHBoxLayout(self.widget_8)
+        self.horizontalLayout_6.setObjectName(u"horizontalLayout_6")
+        self.label_6 = QLabel(self.widget_8)
+        self.label_6.setObjectName(u"label_6")
+        sizePolicy.setHeightForWidth(self.label_6.sizePolicy().hasHeightForWidth())
+        self.label_6.setSizePolicy(sizePolicy)
+        self.label_6.setMinimumSize(QSize(24, 24))
+        self.label_6.setMaximumSize(QSize(24, 26))
+        self.label_6.setPixmap(QPixmap(u":/resources/img/icons/smile.svg"))
+
+        self.horizontalLayout_6.addWidget(self.label_6)
+
+        self.label_7 = QLabel(self.widget_8)
+        self.label_7.setObjectName(u"label_7")
+
+        self.horizontalLayout_6.addWidget(self.label_7)
+
+
+        self.formLayout.setWidget(2, QFormLayout.LabelRole, self.widget_8)
+
+        self.widget_7 = QWidget(self.widget_9)
+        self.widget_7.setObjectName(u"widget_7")
+        self.horizontalLayout_5 = QHBoxLayout(self.widget_7)
+        self.horizontalLayout_5.setObjectName(u"horizontalLayout_5")
+        self.spinBox = QSpinBox(self.widget_7)
+        self.spinBox.setObjectName(u"spinBox")
+        self.spinBox.setMinimumSize(QSize(0, 24))
+        self.spinBox.setFont(font1)
+
+        self.horizontalLayout_5.addWidget(self.spinBox)
+
+        self.label_5 = QLabel(self.widget_7)
+        self.label_5.setObjectName(u"label_5")
+
+        self.horizontalLayout_5.addWidget(self.label_5)
+
+        self.horizontalSpacer_4 = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+
+        self.horizontalLayout_5.addItem(self.horizontalSpacer_4)
+
+
+        self.formLayout.setWidget(2, QFormLayout.FieldRole, self.widget_7)
+
+        self.widget_12 = QWidget(self.widget_9)
+        self.widget_12.setObjectName(u"widget_12")
+        self.horizontalLayout_9 = QHBoxLayout(self.widget_12)
+        self.horizontalLayout_9.setObjectName(u"horizontalLayout_9")
+        self.label_11 = QLabel(self.widget_12)
+        self.label_11.setObjectName(u"label_11")
+        sizePolicy.setHeightForWidth(self.label_11.sizePolicy().hasHeightForWidth())
+        self.label_11.setSizePolicy(sizePolicy)
+        self.label_11.setMinimumSize(QSize(24, 24))
+        self.label_11.setMaximumSize(QSize(24, 24))
+        self.label_11.setPixmap(QPixmap(u":/resources/img/icons/monitor.svg"))
+
+        self.horizontalLayout_9.addWidget(self.label_11)
+
+        self.label_12 = QLabel(self.widget_12)
+        self.label_12.setObjectName(u"label_12")
+
+        self.horizontalLayout_9.addWidget(self.label_12)
+
+
+        self.formLayout.setWidget(3, QFormLayout.LabelRole, self.widget_12)
+
+        self.widget_13 = QWidget(self.widget_9)
+        self.widget_13.setObjectName(u"widget_13")
+        self.gridLayout_5 = QGridLayout(self.widget_13)
+        self.gridLayout_5.setObjectName(u"gridLayout_5")
+        self.comboBox = QComboBox(self.widget_13)
+        self.comboBox.addItem("")
+        self.comboBox.addItem("")
+        self.comboBox.addItem("")
+        self.comboBox.setObjectName(u"comboBox")
+        self.comboBox.setMinimumSize(QSize(0, 24))
+        self.comboBox.setFont(font1)
+
+        self.gridLayout_5.addWidget(self.comboBox, 0, 0, 1, 1)
+
+
+        self.formLayout.setWidget(3, QFormLayout.FieldRole, self.widget_13)
+
+        self.widget_10 = QWidget(self.widget_9)
+        self.widget_10.setObjectName(u"widget_10")
+        self.horizontalLayout_7 = QHBoxLayout(self.widget_10)
+        self.horizontalLayout_7.setObjectName(u"horizontalLayout_7")
+        self.label_8 = QLabel(self.widget_10)
+        self.label_8.setObjectName(u"label_8")
+        sizePolicy.setHeightForWidth(self.label_8.sizePolicy().hasHeightForWidth())
+        self.label_8.setSizePolicy(sizePolicy)
+        self.label_8.setMinimumSize(QSize(24, 24))
+        self.label_8.setMaximumSize(QSize(24, 24))
+        self.label_8.setPixmap(QPixmap(u":/resources/img/icons/clock.svg"))
+
+        self.horizontalLayout_7.addWidget(self.label_8)
+
+        self.label_9 = QLabel(self.widget_10)
+        self.label_9.setObjectName(u"label_9")
+
+        self.horizontalLayout_7.addWidget(self.label_9)
+
+
+        self.formLayout.setWidget(4, QFormLayout.LabelRole, self.widget_10)
+
+        self.widget_11 = QWidget(self.widget_9)
+        self.widget_11.setObjectName(u"widget_11")
+        self.horizontalLayout_8 = QHBoxLayout(self.widget_11)
+        self.horizontalLayout_8.setObjectName(u"horizontalLayout_8")
+        self.spinBox_2 = QSpinBox(self.widget_11)
+        self.spinBox_2.setObjectName(u"spinBox_2")
+        self.spinBox_2.setMinimumSize(QSize(0, 24))
+        self.spinBox_2.setFont(font1)
+
+        self.horizontalLayout_8.addWidget(self.spinBox_2)
+
+        self.label_10 = QLabel(self.widget_11)
+        self.label_10.setObjectName(u"label_10")
+
+        self.horizontalLayout_8.addWidget(self.label_10)
+
+        self.horizontalSpacer_3 = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+
+        self.horizontalLayout_8.addItem(self.horizontalSpacer_3)
+
+
+        self.formLayout.setWidget(4, QFormLayout.FieldRole, self.widget_11)
+
+
+        self.verticalLayout_2.addWidget(self.widget_9)
+
 
         self.verticalLayout.addWidget(self.groupBox_2)
 
@@ -43,6 +272,46 @@ class Ui_MainWindow(object):
         self.groupBox_3.setObjectName(u"groupBox_3")
         self.groupBox_3.setMinimumSize(QSize(0, 100))
         self.groupBox_3.setMaximumSize(QSize(16777215, 100))
+        self.horizontalLayout_2 = QHBoxLayout(self.groupBox_3)
+        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
+        self.toolButton = QToolButton(self.groupBox_3)
+        self.toolButton.setObjectName(u"toolButton")
+        self.toolButton.setMinimumSize(QSize(50, 50))
+        icon = QIcon()
+        icon.addFile(u":/resources/img/icons/arrow-left.svg", QSize(), QIcon.Normal, QIcon.Off)
+        self.toolButton.setIcon(icon)
+        self.toolButton.setIconSize(QSize(40, 40))
+
+        self.horizontalLayout_2.addWidget(self.toolButton)
+
+        self.horizontalSpacer = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+
+        self.horizontalLayout_2.addItem(self.horizontalSpacer)
+
+        self.toolButton_3 = QToolButton(self.groupBox_3)
+        self.toolButton_3.setObjectName(u"toolButton_3")
+        self.toolButton_3.setMinimumSize(QSize(50, 50))
+        icon1 = QIcon()
+        icon1.addFile(u":/resources/img/icons/grid.svg", QSize(), QIcon.Normal, QIcon.Off)
+        self.toolButton_3.setIcon(icon1)
+        self.toolButton_3.setIconSize(QSize(40, 40))
+
+        self.horizontalLayout_2.addWidget(self.toolButton_3)
+
+        self.horizontalSpacer_2 = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+
+        self.horizontalLayout_2.addItem(self.horizontalSpacer_2)
+
+        self.toolButton_2 = QToolButton(self.groupBox_3)
+        self.toolButton_2.setObjectName(u"toolButton_2")
+        self.toolButton_2.setMinimumSize(QSize(50, 50))
+        icon2 = QIcon()
+        icon2.addFile(u":/resources/img/icons/arrow-right.svg", QSize(), QIcon.Normal, QIcon.Off)
+        self.toolButton_2.setIcon(icon2)
+        self.toolButton_2.setIconSize(QSize(40, 40))
+
+        self.horizontalLayout_2.addWidget(self.toolButton_2)
+
 
         self.verticalLayout.addWidget(self.groupBox_3)
 
@@ -64,6 +333,15 @@ class Ui_MainWindow(object):
         self.gridLayout_2.setContentsMargins(0, 0, 0, 0)
         self.groupBox = QGroupBox(self.previewPage)
         self.groupBox.setObjectName(u"groupBox")
+        self.gridLayout_6 = QGridLayout(self.groupBox)
+        self.gridLayout_6.setObjectName(u"gridLayout_6")
+        self.label_13 = QLabel(self.groupBox)
+        self.label_13.setObjectName(u"label_13")
+        self.label_13.setPixmap(QPixmap(u":/resources/img/lul.png"))
+        self.label_13.setScaledContents(True)
+
+        self.gridLayout_6.addWidget(self.label_13, 0, 0, 1, 1)
+
 
         self.gridLayout_2.addWidget(self.groupBox, 0, 0, 1, 1)
 
@@ -97,8 +375,30 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
         self.groupBox_2.setTitle(QCoreApplication.translate("MainWindow", u"Settings", None))
+        self.label_2.setText("")
+        self.label.setText(QCoreApplication.translate("MainWindow", u"API Key", None))
+        self.lineEdit.setPlaceholderText(QCoreApplication.translate("MainWindow", u"Twitch Chat API Key", None))
+        self.label_3.setText("")
+        self.label_4.setText(QCoreApplication.translate("MainWindow", u"Channel", None))
+        self.lineEdit_2.setPlaceholderText(QCoreApplication.translate("MainWindow", u"Twitch Channel Name", None))
+        self.label_6.setText("")
+        self.label_7.setText(QCoreApplication.translate("MainWindow", u"Buffer Size", None))
+        self.label_5.setText(QCoreApplication.translate("MainWindow", u"Emotes", None))
+        self.label_11.setText("")
+        self.label_12.setText(QCoreApplication.translate("MainWindow", u"Mode", None))
+        self.comboBox.setItemText(0, QCoreApplication.translate("MainWindow", u"Auto Cycle", None))
+        self.comboBox.setItemText(1, QCoreApplication.translate("MainWindow", u"Last", None))
+        self.comboBox.setItemText(2, QCoreApplication.translate("MainWindow", u"Manual", None))
+
+        self.label_8.setText("")
+        self.label_9.setText(QCoreApplication.translate("MainWindow", u"Delay", None))
+        self.label_10.setText(QCoreApplication.translate("MainWindow", u"secs", None))
         self.groupBox_3.setTitle(QCoreApplication.translate("MainWindow", u"Control", None))
+        self.toolButton.setText(QCoreApplication.translate("MainWindow", u"...", None))
+        self.toolButton_3.setText(QCoreApplication.translate("MainWindow", u"...", None))
+        self.toolButton_2.setText(QCoreApplication.translate("MainWindow", u"...", None))
         self.groupBox.setTitle(QCoreApplication.translate("MainWindow", u"Preview", None))
+        self.label_13.setText("")
     # retranslateUi
 
 
